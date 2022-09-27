78a416496bb01a71f34168ae01aa41dd3ea7548d
prains <sleskov2013@yandex.ru>
2022-09-27 17:51:11 +0300

    123

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const html = fs.readFileSync('index.html', 'utf-8', () => {});
+
+const normalize = fs.readFileSync('normalize.css', 'utf-8', () => {});
+
+fs.mkdir('../newTemplate', () => {
+  fs.writeFile('../newTemplate/index.html', `${html}`, () => {});
+  fs.writeFile('../newTemplate/index.css', '', () => {});
+  fs.writeFile('../newTemplate/normalize.css', `${normalize}`, () => {})
+  fs.writeFile('../newTemplate/index.js', '', () => {});
+});
