a63cafdb09232ca410d6bd69dad6baa6b48371d6
prains <sleskov2013@yandex.ru>
2022-12-01 23:33:06 +0300

    add scss instead of css

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const normalize = fs.readFileSync('normalize.css', 'utf-8', () => {});
 
 fs.mkdir('../newTemplate', () => {
   fs.writeFile('../newTemplate/index.html', `${html}`, () => {});
-  fs.writeFile('../newTemplate/index.css', '', () => {});
+  fs.writeFile('../newTemplate/index.scss', '', () => {});
   fs.writeFile('../newTemplate/normalize.css', `${normalize}`, () => {})
   fs.writeFile('../newTemplate/index.js', '', () => {});
 });
